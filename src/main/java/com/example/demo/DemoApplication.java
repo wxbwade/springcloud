d9247832948f61a005064c83ca08fe962205b753
LAPTOP-TG84LGLK\songjunyan <wxbwade123@163.com>
2018-08-25 21:45:24 +0800

    配置

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
+@EnableConfigServer
 @SpringBootApplication
 public class DemoApplication {
 
